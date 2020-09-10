a2ae45a397865a2f92796daf6f72fce9396d39fb
Erik Soma <stillusingirc@gmail.com>
2020-09-10 17:42:14 -0400

    First commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+
+[![License: MIT](https://img.shields.io/badge/license-MIT-brightgreen?style=for-the-badge)](https://opensource.org/licenses/MIT)
+
+<h1>wkd</h1>
+
+wkd is a Python library for developing games (or any other media application).
+
+wkd is currently very unstable and incomplete.
+
+## Goals
+
+ * Take advantage of modern Python features including async and subinterpreters
+ * Use the cffi library to allow the library to run on more than CPython
+ * Run on Windows, Linux and Mac
+ * Provide low and high level APIs
+ * Abstract away implementation details and unique behaviors of OS, Graphics
+   APIs, Physics libraries, etc.
+ * Thoroughly test.
+ * Provide handwritten documentation with code examples.
---
+++ setup.py
@@ -0,0 +1,33 @@
+
+# python
+import pathlib
+import setuptools
+
+README_FILE_PATH = pathlib.Path(__file__).parent.absolute().joinpath(
+    'README.md'
+)
+with open(README_FILE_PATH) as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='wkd',
+    version='0.0.1',
+    author='Erik Soma',
+    author_email='stillusingirc@gmail.com',
+    description='Game Development Library',
+    tests_require=['pytest'],
+    install_requires=[],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/esoma/wkd',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Games/Entertainment',
+    ],
+    python_requires='>=3.9',
+)
